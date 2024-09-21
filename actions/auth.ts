4721e28b4aff7ff89aeffe2dec10eb2a86239bc6
Mike Pham <mikep@xello.world>
2024-09-21 01:46:59 -0400

    feat: Create register user function

---
+++ actions/auth.ts
@@ -0,0 +1,29 @@
+'use server' // this is a server action
+
+import { cookies } from 'next/headers'
+import { signin, signup } from '@/utils/authTools'
+import { z } from 'zod'
+import { redirect } from 'next/navigation'
+import { COOKIE_NAME } from '@/utils/constants'
+
+const authSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+export const registerUser = async (previousState, formData: FormData) => {
+  const data = authSchema.parse({
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+
+  try {
+    const { token } = await signup(data)
+    cookies().set(COOKIE_NAME, token)
+  } catch (error) {
+    console.log(error)
+    return { message: 'Failed to sign you up' }
+  }
+
+  redirect('/dashboard')
+}
